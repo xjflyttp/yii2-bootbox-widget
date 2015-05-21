f223b6bbe02214f5ab9384469f2a07f239cd822d
David-Julian BUCH <buchdj@gmail.com>
2015-05-21 10:13:07 +0200

    Allow special registration
    With automatic confirm override and locale setting

--- BootboxAsset.php
+++ BootboxAsset.php
@@ -7,6 +7,7 @@
 
 namespace xj\bootbox;
 
+use \Yii;
 use yii\web\AssetBundle;
 
 class BootboxAsset extends AssetBundle {
@@ -17,5 +18,28 @@ class BootboxAsset extends AssetBundle {
     public $depends = [
         'yii\bootstrap\BootstrapPluginAsset'
     ];
+    
+    /**
+     * Registers this asset bundle with a view.
+     * @param \yii\web\View $view the view to be registered with
+     * @return static the registered asset bundle instance
+     */
+    public static function registerWithOverride($view)
+    {
+        $assetbundle = self::register($view);
+        Yii::$app->view->registerJs('
+            yii.confirm = function(message, ok, cancel) {
+                bootbox.confirm(message, function(result) {
+                    if (result) { !ok || ok(); } else { !cancel || cancel(); }
+                });
+            }
+        ');
+
+        if(Yii::$app->language !== null && strlen(Yii::$app->language)>=2) {
+            Yii::$app->view->registerJs('bootbox.setDefaults({locale: "'.substr(Yii::$app->language,0,2).'"});');
+        }
+        
+        return $assetbundle;
+    }
 
 }
